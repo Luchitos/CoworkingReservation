f185b1aa2ff413617bbc28176a3c0bed1e95e1a0
lucianosignorellibueedu <luchitosignorelli@gmail.com>
2024-12-30 00:48:25 -0300

    Removed using innecesarios

--- CoworkingReservation.Domain/Entities/CoworkingSpace.cs
+++ CoworkingReservation.Domain/Entities/CoworkingSpace.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoworkingReservation.Domain.Entities
+﻿namespace CoworkingReservation.Domain.Entities
 {
     public class CoworkingSpace
     {
--- CoworkingReservation.Domain/Entities/User.cs
+++ CoworkingReservation.Domain/Entities/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoworkingReservation.Domain.Entities
+﻿namespace CoworkingReservation.Domain.Entities
 {
     public class User
     {
