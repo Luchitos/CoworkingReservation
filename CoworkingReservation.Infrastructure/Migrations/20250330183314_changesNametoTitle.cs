1385bd106a388e09d6f649a622ad7375b057a61f
Luciano Signorelli <luchitosignorelli@gmail.com>
2025-03-30 15:35:17 -0300

    Changes Name to Title in CoworkingSpace

---
+++ CoworkingReservation.Infrastructure/Migrations/20250330183314_changesNametoTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoworkingReservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changesNametoTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "CoworkingSpaces",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "CoworkingSpaces",
+                newName: "Name");
+        }
+    }
+}
